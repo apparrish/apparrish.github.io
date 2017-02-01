c9081483b3c406d36c9151c047d41ee64fa86187
apparrish <austen.p.parrish@gmail.com>
2017-01-31 23:03:58 -0500

    added formspring AJAX submission

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,14 +20,15 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
-                type: "POST",
+                url: "//formspree.io/johannaobenda@gmail.com",
+                method: "POST",
                 data: {
                     name: name,
                     phone: phone,
                     email: email,
                     message: message
                 },
+                datatype: "json",
                 cache: false,
                 success: function() {
                     // Success message
