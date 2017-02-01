57bd76ac9c736c4b88873e9aa32df2c5f0726cac
apparrish <austen.p.parrish@gmail.com>
2017-01-31 23:20:38 -0500

    addressed Access-Control-Allow-Origin problem

--- js/contact_me.js
+++ js/contact_me.js
@@ -20,7 +20,7 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "//formspree.io/johannaobenda@gmail.com",
+                url: "//www.formspree.io/johannaobenda@gmail.com",
                 method: "POST",
                 data: {
                     name: name,
